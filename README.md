2fb51181c6356d422ecb0a5b3e1efa10f4a9fbbf
freewind <nowindlee@gmail.com>
2018-08-27 16:18:39 +0800

    demo

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
-Vue Typescript Demo
-===================
+Vue Typescript Getter Calculated Once Demo
+==========================================
 
+The `getter` method in a vue component is calculated only once (that's good).
+
+Display many times:
+
+![demo1.jpg](./images/demo1.jpg)
+
+Only calculated once:
+
+![demo2.jpg](./images/demo2.jpg)
+
+Run
+---
 
 ```
 npm install
 npm run serve
 ```
+
+Open browser's developer console, then change the content of the textfield, watch the log in developer console.
--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,47 @@
 <template>
-    <HelloWorld/>
+    <div>
+        <input v-model="message" autofocus/>
+        <div>{{ message }}</div>
+        <div>Open browser's developer console to see the calculation log</div>
+        <ul>
+            <li>char count: {{ messageCharCount }}</li>
+            <li>char count: {{ messageCharCount }}</li>
+            <li>char count: {{ messageCharCount }}</li>
+            <li>char count: {{ messageCharCount }}</li>
+            <li>char count: {{ messageCharCount }}</li>
+            <li>char count: {{ messageCharCount }}</li>
+            <li>char count: {{ messageCharCount }}</li>
+            <li>char count: {{ messageCharCount }}</li>
+            <li>char count: {{ messageCharCount }}</li>
+            <li>char count: {{ messageCharCount }}</li>
+            <li>char count: {{ messageCharCount }}</li>
+            <li>char count: {{ messageCharCount }}</li>
+            <li>char count: {{ messageCharCount }}</li>
+            <li>char count: {{ messageCharCount }}</li>
+            <li>char count: {{ messageCharCount }}</li>
+            <li>char count: {{ messageCharCount }}</li>
+            <li>char count: {{ messageCharCount }}</li>
+            <li>char count: {{ messageCharCount }}</li>
+            <li>char count: {{ messageCharCount }}</li>
+            <li>char count: {{ messageCharCount }}</li>
+            <li>char count: {{ messageCharCount }}</li>
+            <li>char count: {{ messageCharCount }}</li>
+            <li>char count: {{ messageCharCount }}</li>
+        </ul>
+    </div>
 </template>
 
 <script lang="ts">
-    import {Component, Vue} from 'vue-property-decorator';
-    import HelloWorld from './components/HelloWorld.vue';
+    import {Component, Vue} from 'vue-property-decorator'
 
-    @Component({
-        components: {
-            HelloWorld,
-        },
-    })
+    @Component({})
     export default class App extends Vue {
+        message: string = 'change me'
+
+        get messageCharCount(): number {
+            const now = new Date().getTime()
+            console.log('calculate char count at ' + now)
+            return this.message.length
+        }
     }
 </script>
